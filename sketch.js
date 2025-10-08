102b904c4f429106561d21223792c0d6db098065
ann-eats-a-soul-ack <72539248+ann-eats-a-soul-ack@users.noreply.github.com>
2025-10-08 19:32:58 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,18 +27,18 @@ createCanvas(windowWidth, windowHeight);
   
  c1 = color(255);
   c2 = color(30, 0, 255);
-  
-  for(let y=0; y<height; y++){
+}
+
+function draw() {
+//  background(255);
+  showResetButton();
+
+    for(let y=0; y<height; y++){
     n = map(y,0,height,0,1);
     let newc = lerpColor(c1,c2,n);
     stroke(newc);
     line(0,y,width, y);
   }  
-}
-
-function draw() {
-  background(255);
-  showResetButton();
   
   fill(255);
   textSize(25);
